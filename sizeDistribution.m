c3167bab5804be9adc063e9415ae6a8396429211
nils <nils.haentjens@maine.edu>
2017-01-16 19:39:26 -0500

    Added timeseries plot of size distribution

--- sizeDistribution.m
+++ sizeDistribution.m
@@ -12,7 +12,7 @@ fprintf('Done\n');
 % 1. Parameters
 % Feature to load
 % 2:Area    3:BioVolume
-% size is estimated from BioVolume (column=3; name='Diameter')
+% Diameter is estimated from BioVolume (column=3; name='Diameter')
 cfg.feature.name = 'Diameter';
 cfg.feature.units = '\mum';
 cfg.feature.column = 3;
@@ -20,7 +20,8 @@ cfg.feature.import = false;
 % Bin to plot
 % cfg.bin.dt.begin_str='D20160511T153155';
 % cfg.bin.dt.end_str='D20160604T155803';
-cfg.bin.selection = [cfg.path.wk 'selection/inline.csv'];
+cfg.bin.selection = [cfg.path.wk 'selection/station_inline_4.csv']; % inline.csv
+cfg.bin.period = 'May 2016';
 cfg.plot.normalize = true;
 cfg.plot.n_max = 20;
 % cfg.plot.Dmin = 700; % minimum is 1
@@ -31,6 +32,9 @@ cfg.plot.Dauto = false; % if true does not take Dmin and Dmax into account
 cfg.plot.x_log_scale = true;
 cfg.plot.y_log_scale = false;
 % cfg.plot.y_lim = [1; 2e6];
+cfg.plot.save = false;
+cfg.export.save = false;
+cfg.export.name = 'stn4';
 
 % 2. Import/Load features
 if cfg.feature.import
@@ -38,7 +42,7 @@ if cfg.feature.import
   cfg.path.wk_features=[cfg.path.wk 'features' filesep];
   bins=dir([cfg.path.wk_features 'D*_fea_v2.csv']);
   bins={bins(:).name}; n = size(bins,2);
-  bin=ftr; dt=NaN(n,1); ftr=cell(n,1);
+  bin=cell(n,1); dt=NaN(n,1); ftr=cell(n,1);
   range2read = [1 (cfg.feature.column - 1) -1 (cfg.feature.column - 1)];
   if cfg.proc.parallel
     parfor i=1:n
@@ -68,17 +72,17 @@ else
   fprintf('Done\n');
 end;
 
-% 3. Plot
+% 3. Selecting data
 fprintf('Filtering... ');
 % Get selection of bin
 if isfield(cfg.bin, 'selection')
   % load file with selection
   f = fopen(cfg.bin.selection);
   data = textscan(f, '%s', 'Delimiter', ',');
-  fclose(f);
+  fclose(f); data = data{1};
   sel = [];
-  for i = 1:size(bin,1);
-    foo = find(not(cellfun('isempty',(strfind(data{1}, bin{i})))));
+  for i = 1:size(data,1);
+    foo = find(not(cellfun('isempty',(strfind(bin, data{i})))));
     if ~isempty(foo); sel(end+1,1) = foo; end;
   end;
 elseif isfield(cfg.bin, 'dt')
@@ -91,6 +95,8 @@ else
 end;
 fprintf('Done\n');
 
+% 4. Plot histogram
+fprintf('Plotting histogram...');
 % Get min and max of size
 if cfg.plot.Dauto
   Dmin = ftr{sel(1)}(1); Dmax = ftr{sel(1)}(1);
@@ -110,8 +116,8 @@ D=Dmin*q.^N; D(end) = D(end) * 1.000000001;
 ftr_all = [];
 for i=sel'; ftr_all(end + 1:end + size(ftr{i},1)) = ftr{i}; end;
 % Plot histogram
-% figure(10);
-hold('on');
+figure(10); clf(10);
+% hold('on');
 if cfg.plot.normalize;
   histogram(ftr_all, D, 'Normalization', 'probability');
   ylabel('Normalized (number of cells)');
@@ -132,10 +138,53 @@ if cfg.plot.y_log_scale;
   y_lim = ylim(); set(gca,'YScale','log'); ylim(y_lim);
 end;
 fprintf('Done\n');
+% Save figure
+if cfg.plot.save;
+  dir_fig = [cfg.path.wk 'fig/'];
+  if ~isdir(dir_fig); mkdir(dir_fig); end;
+  set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 21 21]);
+  print('-dpng', '-r300', sprintf('%s/SizeDistributionNormalized', dir_fig));
+end;
 
-return
-%% Save figure
-dir_fig = [cfg.path.wk 'fig/'];
-if ~isdir(dir_fig); mkdir(dir_fig); end;
-set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 21 21]);
-print('-dpng', '-r300', sprintf('%s/SizeDistributionNormalized', dir_fig));
+%% 5. Plot time series
+fprintf('Plotting time series...');
+figure(11); clf(11);
+% Compute median of feature for each bin
+y = []; y_std = []; y_pt25 = []; y_pt75=[]; n=[]; y_tot=[];
+for i=sel';
+  y(end+1,1) = median(ftr{i});
+  y_std(end+1,1) = std(ftr{i});
+  y_pt25(end+1,1)=prctile(ftr{i}, 25);
+  y_pt75(end+1,1)=prctile(ftr{i}, 75);
+  y_tot(end+1, 1)= sum(ftr{i});
+  n(end+1,1) = size(ftr{i},1);
+end;
+% Plot left
+yyaxis('left')
+plot(dt(sel), y, '--o');
+% hold('on'); plot(dt(sel), y_pt25, dt(sel), y_pt75);
+% errorbar(dt(sel), y, y_std);
+ylabel(['Median ' cfg.feature.name ' (' cfg.feature.units ')' ]);
+% Plot number of ROI
+yyaxis('right');
+plot(dt(sel), n, 'd');
+datetick('x', 'ddd-HH:MM');
+set(gca, 'XTickLabelRotation', 45);
+ylabel('Number of ROI');
+% xlim([datenum(2016,05,24,04,00,00), datenum(2016,05,29,03,00,00)]);
+xlabel(cfg.bin.period);
+% Save figure
+fprintf('Done\n');
+if cfg.plot.save;
+  dir_fig = [cfg.path.wk 'fig/'];
+  if ~isdir(dir_fig); mkdir(dir_fig); end;
+  set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 21 15]);
+  print('-dpng', '-r300', sprintf('%s/SizeDistributionOverTime', dir_fig));
+end;
+
+%% Export data
+if cfg.export.save
+  x_dt=dt(sel);
+  y_med=y;
+  save([cfg.path.wk 'sizeDistributionTS_' cfg.feature.name '_' cfg.export.name], 'x_dt', 'y_*', 'n');
+end;
