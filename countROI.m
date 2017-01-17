be309565dc1e7891aae377a36f9444108337675c
nils <nils.haentjens@maine.edu>
2017-01-17 10:53:55 -0500

    Update countROI

--- countROI.m
+++ countROI.m
@@ -9,31 +9,31 @@ addpath('helpers');
 cfg = loadCfg(cfg.filename);
 fprintf('Done\n');
 
-% 1. Parameters
-cfg.counts.import = false;
-cfg.meta.period = 'November-December 2016';
-cfg.selection.name = 'Inline';
-cfg.path.selection = [cfg.path.wk 'selection/inline.csv'];
+%% 1. Parameters
+cfg.counts.import = true;
+cfg.counts.export2csv = false;
+cfg.path.wk_selection = [cfg.path.selection cfg.process.selection];
 
-% 2. Run through all the files
-if cfg.counts.import;
+%% 2. Run through all the files
+if cfg.counts.import
   fprintf('Importing counts... ');  tic;% ~2 min
   bins=dir([cfg.path.in 'D*.adc']);
   bins={bins(:).name};
   dt=NaN(size(bins,2),1); counts=dt; bin={};
   if cfg.proc.parallel
-    parfor i=1:size(bins,2);
+    parfor i=1:size(bins,2)
       bin{i,1} = bins{i}(1:24);
       dt(i,1)=datenum(bins{i}(2:16), 'yyyymmddTHHMMSS');
       counts(i,1)=getNumberROI([cfg.path.in bins{i}]);
     end;
   else
-    for i=1:size(bins,2);
+    for i=1:size(bins,2)
       bin{i,1} = bins{i}(1:24);
       dt(i,1)=datenum(bins{i}(2:16), 'yyyymmddTHHMMSS');
       counts(i,1)=getNumberROI([cfg.path.in bins{i}]);
     end;
   end;
+  if ~isdir(cfg.path.wk); mkdir(cfg.path.wk); end;
   save([cfg.path.wk 'counts'], 'bin', 'dt', 'counts');
   fprintf('Done\n'); toc;  
 else
@@ -42,15 +42,15 @@ else
   fprintf('Done\n');
 end;
 
-% 3. Get selection
-sel = getSelection(bin, cfg.path.selection);
+%% 3. Get selection
+sel = getSelection(bin, cfg.path.wk_selection);
 dt_sel=[]; counts_sel=[];
-for i=sel';
+for i=sel'
   dt_sel(end + 1:end + size(dt(i),1),1) = dt(i);
   counts_sel(end + 1:end + size(counts(i),1),1) = counts(i);
 end;
 
-% 4. Quick plots
+%% 4. Quick plots
 figure(11); clf(11);
 plot(dt_sel, counts_sel, 'o--');
 title([cfg.meta.cruise ' - ' cfg.meta.instrument]);
@@ -60,7 +60,10 @@ datetick('x', 'dd'); xlabel(cfg.meta.period);
 % Display number of ROI
 fprintf('Number of ROI: %d\n', sum(counts_sel));
 
-return
+
 %% 5. Export counts
-cfg.path.wk_counts=[cfg.path.wk 'CountROI_' cfg.selection.name '.csv'];
-csvwrite(cfg.path.wk_counts, [datevec(dt_sel), counts_sel]);
+if cfg.counts.export2csv 
+  if ~isdir(cfg.path.wk); mkdir(cfg.path.wk); end;
+  cfg.path.wk_counts = [cfg.path.wk 'countROI_' cfg.process.selection_name '.csv'];
+  csvwrite(cfg.path.wk_counts, [datevec(dt_sel), counts_sel]);
+end;
